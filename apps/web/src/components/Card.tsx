e483521ceb559f75476149a1b7d18efdb07bb0bd
Jonathan Mangarret <jmangarret19@gmail.com>
2026-01-05 16:47:13 -0400

    Remove unused components (Navbar, Button, Card, Input)

--- apps/web/src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React, { HTMLAttributes } from 'react';
-
-export const Card: React.FC<HTMLAttributes<HTMLDivElement>> = ({ className = '', children, ...props }) => {
-    return (
-        <div className={`card ${className}`} {...props}>
-            {children}
-        </div>
-    );
-};
