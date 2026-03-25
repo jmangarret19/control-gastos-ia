99d7ed5c1712ffd15c31d4855cb0c810add90a38
Jonathan Mangarret <jmangarret19@gmail.com>
2026-03-25 03:42:02 -0400

    feat: add Android platform to the web application using Capacitor.

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jmangarret.controlgastos',
+  appName: 'ControlGastos',
+  webDir: 'dist'
+};
+
+export default config;
